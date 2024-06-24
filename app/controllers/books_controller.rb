e2d9b9748e273512245ae8b22fac4efc1c3abb2c
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-06-24 21:44:07 +0000

    20240625AM　create機能まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,32 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @book = Book.new
+    @books = Book.all  
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'List was successfully created.'
+    else
+      render :new
+    end
   end
 
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end

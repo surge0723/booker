3e2570f2528cb9e6ad2db4fe15d9e007c1b94d85
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-06-27 15:18:44 +0000

    テスト前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,12 +11,12 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     if @book.save
-      flash[:notice]="List was successfully created."
+      flash[:notice] = "Book was successfully created."
       redirect_to @book
     else
-      flash[:alert]="投稿ミス"
+      @books=Book.all
       render :index
-    end
+  end
   end
 
   def show
@@ -34,9 +34,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book=Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render:edit
     end
   end
 
